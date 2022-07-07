bfd2dcbf4de4c3635521a027b95273595cc46d75
梁先华 <909901234@qq.com>
2022-07-07 10:34:33 +0800

    Revert "引入getx辅助工具创建文件,后续文件名格式更改;"
    This reverts commit 1a13077ab3de56f017fead972d567d74fe615962.

--- lib/ui/splash_page/splash_page.dart
+++ lib/ui/splash_page/splash_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_palyer_demo/ui/splash_page/splash_anim_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
